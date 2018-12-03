8c9251410797afe64dc7338c5774c08f7d83a293
Artur Hoffman <arthof@wp.pl>
2018-12-03 22:28:42 +0100

    Create basic post controller.

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,16 +4,63 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Post;
 
 class PostController extends AbstractController
 {
+    
     /**
-     * @Route("/post", name="post")
+     * @Route("/posts/", name="posts")
      */
-    public function index()
+    public function list(Request $request)
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        $response = new Response();
+        
+        $em = $this->getDoctrine()->getManager();
+        $postRepository = $em->getRepository(Post::class);
+        $posts = $postRepository->findAll();
+        
+        
+        $outputHtml = 'List of all posts:<br>';
+        
+        if($posts)
+        {
+            foreach($posts as $post)
+            {
+                $outputHtml.='ID: ' . $post->getId() . '; Title: ' . $post->getTitle() . '<br>';
+            }
+        }
+
+        $response->setContent($outputHtml);        
+        return $response;
+    }
+    
+    
+    /**
+     * @Route("/post/add/", name="post_add")
+     */
+    public function index(Request $request)
+    {
+        $response = new Response();
+
+        $title = $request->request->get('title', '');
+        $content = $request->request->get('content', '');
+        
+        if(!strlen($title))
+        {
+            $response->setContent('Title can\'t be empty!');
+            return $response;
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        $post = new Post($title, $content);
+        
+        $em->persist($post);
+        $em->flush();
+        
+        $response->setContent('Post created, id: ' . $post->getId());
+        return $response;
     }
 }
